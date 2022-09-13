9fd3313586045e4b8d345e99680fe8e58a327863
ji914 <112610050+ji914@users.noreply.github.com>
2022-09-13 22:21:21 +0900

    Add files via upload

---
+++ 20220908/20220908_pjh_test2/20220908_pjh_test2/20220908_pjh_test2.cpp
@@ -0,0 +1,66 @@
+﻿// 20220908_pjh_test2.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main(void)
+{
+	int num1 = 12; 
+	int num2 = 12;
+	printf("num1 ; %d \n", num1);
+	printf("num1++ ; %d \n", num1++);
+	printf("num1 ; %d \n", num1);
+	printf("num1++ ; %d \n", ++num1);
+
+
+
+	return 0;
+
+}
+
+/*
+int num1=3, num2=4;
+	int result1 = num1 + num2;
+	int result2 = num1 - num2;
+	int result3 = num1 * num2;
+	int result4 = num1 / num2;
+	int result5 = num1 % num2;
+
+printf("num1; %d, num2; %d \n", num1, num2);
+	printf("%d + %d= %d \n", num1, num2, result1);
+	printf("%d - %d= %d \n", num1, num2, result2);
+	printf("%d * %d= %d \n", num1, num2, result3);
+	printf("%d / %d= %d(몫) \n", num1, num2, result4);
+	printf("%d / %d= %d(나머지) \n", num1, num2, result5);
+	return 0;
+*/
+
+/*
+    int num1, num2;
+	int num3 = 30, num4 = 40;
+
+	num1 = num2 = 0;
+
+	printf("num1; %d, num2; %d \n", num1, num2);
+	printf("num3; %d, num4; %d \n", num3, num4);
+
+	return 0;
+	
+	*/
+
+/*
+    int num;
+	num = 20;
+	printf("%d \n", num);
+	*/
+
+// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
+// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+
+// 시작을 위한 팁: 
+//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
+//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
+//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
+//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
+//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
+//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
