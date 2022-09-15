34c3afd394618959f8e726c4008aac063936401f
ji914 <112610050+ji914@users.noreply.github.com>
2022-09-15 16:31:09 +0900

    Add files via upload

---
+++ 20220915/20220915_pjh_test4/20220915_pjh_test4/20220915_pjh_test4.cpp
@@ -0,0 +1,20 @@
+﻿// 20220915_pjh_test4.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <stdio.h>
+int main()
+{
+	char ch1 = 'A', ch2 = 0x41;
+	int ch3 = 'Z', ch4 = 90;
+
+	printf("%c %d \n", ch1, ch1);
+	printf("%c %d \n", ch2, ch2);
+	printf("%c %d \n", ch3, ch3);
+	printf("%c %d \n", ch4, ch4);
+
+	return 0;
+
+}
