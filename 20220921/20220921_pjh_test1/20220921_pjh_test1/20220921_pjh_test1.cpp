53d9fad15b73b99ab4d9167361346ae9767d07fb
ji914 <112610050+ji914@users.noreply.github.com>
2022-09-21 12:02:09 +0900

    Add files via upload

---
+++ 20220921/20220921_pjh_test1/20220921_pjh_test1/20220921_pjh_test1.cpp
@@ -0,0 +1,22 @@
+﻿// 20220921_pjh_test1.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <stdio.h>
+int main(void)
+{
+	double num1 = 245;
+	int num2 = 3.1415;
+	int num3 = 129;
+	char ch = num3;
+	
+	printf("정수 245를 실수로: %f \n", num1);
+	printf("실수 245를 정수로: %d \n", num2);
+	printf("큰 정수 245를 작은 정수로: %d \n", ch);
+
+
+	return 0;
+
+}
---
+++ 20220921/20220921_pjh_test4/20220921_pjh_test4/20220921_pjh_test4.cpp
@@ -0,0 +1,21 @@
+﻿// 20220921_pjh_test4.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <stdio.h>
+ 
+int main()
+{
+	int a = 0;
+
+	printf("정수를 입력하세요. ");
+	scanf("%d", &a);
+	printf("입력된 정수의 10진수는 %d 입니다.\n ", a);
+	printf("입력된 정수의 16진수는 %x 입니다.\n ", a);
+	printf("입력된 정수의 8진수는 %o 입니다.\n ", a);
+
+
+	return 0;
+}
