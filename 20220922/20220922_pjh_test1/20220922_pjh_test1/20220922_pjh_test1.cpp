e5ae42a858176fdf324fa2f4a9205d77e337da76
ji914 <112610050+ji914@users.noreply.github.com>
2022-09-22 17:05:26 +0900

    Add files via upload

---
+++ 20220922/20220922_pjh_test1/20220922_pjh_test1/20220922_pjh_test1.cpp
@@ -0,0 +1,27 @@
+﻿
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <stdio.h>
+
+int main(void)
+{
+	int num = 0;
+	int i = 0;
+	int result = 0;
+	printf("구구단 단수를 입력하세요.");
+	scanf("%d", &i);
+
+	while (num < 10)
+
+	{
+		result = i * num;
+		printf("구구단: %d x %d = %d \n", i, num, result);
+		num++;
+
+	}; 
+	
+
+	return 0;
+}; // while (num < 10)
