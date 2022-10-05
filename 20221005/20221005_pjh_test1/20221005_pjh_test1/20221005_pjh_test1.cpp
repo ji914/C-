048e01ed37a3bc7b2c60d342b53a91ffe5dcb2b1
ji914 <112610050+ji914@users.noreply.github.com>
2022-10-05 12:04:32 +0900

    Add files via upload

---
+++ 20221005/20221005_pjh_test1/20221005_pjh_test1/20221005_pjh_test1.cpp
@@ -0,0 +1,43 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+	int NUM = 0;
+	int i = 0, sum = 0;
+	printf("input number");
+	scanf("%d", &NUM);
+
+	for (i = 0; i <= NUM; i++)
+	{
+		sum += i;
+	}
+
+	printf("1에서 %d까지의 합은 %d 입니다.\n", NUM, sum);
+
+	i = 0;
+	sum = 0;
+	while (i <= NUM)
+	{
+		sum += i;
+		i++;
+	}
+	
+	printf("1에서 %d까지의 합은 %d 입니다.\n", NUM, sum);
+
+
+	i = 0;
+	sum = 0;
+	do 
+	{
+		sum += i;
+		i++;
+	} while (i <= NUM);
+	
+	printf("1에서 %d까지의 합은 %d 입니다.\n", NUM, sum);
+
+	return 0;
+}
+
---
+++ 20221005/20221005_pjh_test2/20221005_pjh_test2/20221005_pjh_test2.cpp
@@ -0,0 +1,44 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+
+  
+	int i = 1;
+	int NUM = 0;
+	int sum = 2;
+	printf("input number ");
+	scanf("%d", &NUM);
+
+	for (sum = 2; sum < NUM; i++ )
+	{
+		printf("%d + %d =",sum, i);
+		sum = sum + i;
+		printf("%d \n", sum);
+	}
+
+	sum = 2;
+	while (i < NUM)
+	{
+		printf("%d +%d = ", sum, i);
+		sum = sum + i;
+		printf("%d \n", sum);
+		
+	}
+
+	sum = 2;
+	do
+	{
+		printf("%d + %d =", sum, i);
+		sum = sum + i;
+		printf("%d \n", sum);
+		
+	} while (i < NUM);
+
+
+	return 0;
+}
+
