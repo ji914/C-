ef300d8751bf6cd3e3d9136240087fa8086538e7
ji914 <112610050+ji914@users.noreply.github.com>
2022-10-13 16:33:41 +0900

    Add files via upload

---
+++ 20221013/20221013_pjh_test1/20221013_pjh_test1/20221013_pjh_test1.cpp
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <stdio.h>
+
+int main(void)
+{
+	int num = 0, abs = 0;
+	printf("정수 입력: ");
+	scanf("%d", &num);
+
+	 abs = num > 0 ? num : num * (-1);
+	 printf("절대값: %d \n", abs);
+	
+	
+	if (num>0)
+	{
+		abs = num;
+		printf("절대값: %d \n", abs);
+	}
+	
+	else 
+	{
+		abs = num * (-1);
+	}
+     printf("절대값: %d \n", abs);
+
+	return 0;
+}
