5c2846e3104946f070a8efdfc1e5c38e77b2563b
ji914 <112610050+ji914@users.noreply.github.com>
2022-11-16 12:00:00 +0900

    Add files via upload

---
+++ 20221116/20221116_pjh_test1/20221116_pjh_test1/20221116_pjh_test1.cpp
@@ -0,0 +1,73 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+
+	FILE* fp;
+
+	fp = fopen("data.txt", "wt");
+
+	if (fp == NULL)
+	{
+		printf("File does not exit!\n");
+		return -1;
+	}
+
+	fprintf(fp, "test\n");
+	fputc('A', fp);
+	fputc('B', fp);
+	fputc('C', fp);
+	fclose(fp);
+
+	fp = fopen("data.txt", "rt");
+	if (fp == NULL)
+	{
+		printf("File does not exit!\n");
+		return -1;
+	}
+	char ch;
+	int i;
+	for (i=0; i<8; i++)
+	{
+       ch = fgetc(fp);
+	   printf("%c", ch);
+	}
+	fclose(fp);
+	
+
+	return 0;
+}
+
+
+
+
+
+
+/*
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+
+	FILE* fp;
+
+	fp = fopen("data.txt", "wt");
+
+	if (fp == NULL)
+	{
+		printf("File does not exit!\n");
+		return -1;
+	}
+
+	fprintf(fp, "test\n");
+	fclose(fp);
+
+	return 0;
+}
+*/
---
+++ 20221116/20221116_pjh_test2/20221116_pjh_test2/20221116_pjh_test2.cpp
@@ -0,0 +1,34 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+	FILE* fp;
+	int a, b;
+	int i = 0; int sum = 0; double avg = 0.0;
+	fp = fopen("d:\\input.txt", "r");
+	if (fp == NULL)
+	{
+		printf("File does not Exit!\n");
+		return -1;
+	}
+	/*
+	fscanf(fp,"%d",&a);
+	fscanf(fp,"%d",&b);
+	printf("%d + %d = %d\n",a,b,a+b);
+	printf("%d - %d = %d\n", a, b, a-b);
+	printf("%d x %d = %d\n", a, b, a*b);
+	printf("%d / %d = %d\n", a, b, a/b);
+     */
+	for ( i = 0; i < 6; i++)
+	{
+		fscanf(fp, "%d", &a);
+		sum += a;
+	}
+	avg = sum / 6.0; printf("avg = %lf\n", avg);
+
+	return 0;
+}
+
